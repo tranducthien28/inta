3b7784915541452a835d61a9e91b16575c0175ba
tranducthien28 <tranthien242000@gmail.com>
2022-11-28 17:00:01 +0700

    xyz

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function () {
     $('.slide').owlCarousel({ loop: 1, autoplay: 1, items: 1, dots: 1, nav: 0})
     $('.pro-cnt').owlCarousel({ loop: 1, autoplay: 1, items: 3, dots: 1, nav: 0, margin: 25})
     $('.pro-newest').owlCarousel({ loop: 1, autoplay: 1, items: 1, dots: 1, nav: 0})
+    $('.newsest-slide').owlCarousel({ loop: 1, autoplay: 1, items: 3, margin: 25, dots: 0, nav: 1, navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']})
     
     const resizeImage = (e, s) => { s = parseInt($(e).width()) * s; $(e).css({ height: s + "px" }) };
     resizeImage('.news .right .news-item .img', 100/170);
