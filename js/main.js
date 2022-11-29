45aaadb9fa59fd87875072ff55aef5be18a318cf
tranducthien28 <tranthien242000@gmail.com>
2022-11-29 16:59:03 +0700

    abc

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function () {
     $('.slide').owlCarousel({ loop: 1, autoplay: 1, items: 1, dots: 1, nav: 0})
     $('.pro-cnt').owlCarousel({ loop: 1, autoplay: 1, items: 3, dots: 1, nav: 0, margin: 25})
     $('.pro-newest').owlCarousel({ loop: 1, autoplay: 1, items: 1, dots: 1, nav: 0})
-    $('.newsest-slide').owlCarousel({ loop: 1, autoplay: 1, items: 3, margin: 25, dots: 0, nav: 1, navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']})
+    $('.newsest-slide').owlCarousel({ loop: 1, autoplay: 1, items: 3, margin: 25, dots: 0, nav: 1, navText: ['<img src="image/left.png" alt="">','<img src="image/right.png" alt="">']})
     
     const resizeImage = (e, s) => { s = parseInt($(e).width()) * s; $(e).css({ height: s + "px" }) };
     resizeImage('.news .right .news-item .img', 100/170);
@@ -17,5 +17,13 @@ $(function () {
     $('.question .answer-item .que').click(function(){
         $(this).siblings('.desc').slideToggle();
         $(this).toggleClass('active');
+    })
+
+    $('.lst-tab li').click(function(){
+        var index  = $(this).data('index');
+        $('.lst-tab li').removeClass('active');
+        $(this).addClass('active');
+        $('.content-tab .item').hide();
+        $('.content-tab #' + index).show();
     })
 })
